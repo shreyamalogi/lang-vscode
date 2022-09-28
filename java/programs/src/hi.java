d1fe7c936ca2b3ec0055ec5ae2b4d37897157ffe
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-09-29 01:06:41 +0530

    first commit

---
+++ java/programs/src/hi.java
@@ -0,0 +1,12 @@
+//package io.github.jiangdequan;
+
+public class hi {
+    
+        // Your program begins with a call to main().
+        // Prints "Hello, World" to the terminal window.
+        public static void main(String args[])
+        {
+            System.out.println("Hello, World");
+        }
+    
+}
